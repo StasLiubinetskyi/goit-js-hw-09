6bc7c2a07ec2e59b62de1f4fa7b0053323533348
Станіслав Любінецький <121062371+StasLiubinetskyi@users.noreply.github.com>
2023-05-27 21:30:45 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
 
+let intervalId;
+
+startButton.addEventListener('click', startColorSwitching);
+stopButton.addEventListener('click', stopColorSwitching);
+
+function startColorSwitching() {
+  startButton.disabled = true; // вимкнути кнопку "Start"
+  intervalId = setInterval(changeBackgroundColor, 1000);
+}
+
+function stopColorSwitching() {
+  startButton.disabled = false; // увімкнути кнопку "Start"
+  clearInterval(intervalId);
+}
+
+function changeBackgroundColor() {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
